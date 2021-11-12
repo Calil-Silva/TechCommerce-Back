c58930a1446576f80980a8f8dd03d0ada175d166
zeus.k99@gmail.com <zeus.k99@gmail.com>
2021-11-12 16:33:54 -0400

    add product test

--- src/controllers/categories.js
+++ src/controllers/categories.js
@@ -3,7 +3,7 @@ import connection from '../database/database.js';
 export default async function categories(req, res) {
   try {
     const result = await connection.query(`
-        SELECT name, url_image
+        SELECT *
         FROM categories;`);
     return res.status(200).send(result.rows);
   } catch (error) {
