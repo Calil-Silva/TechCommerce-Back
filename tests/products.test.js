2a15dfc1750048a081975fe5a4870a3179d21800
zeus.k99@gmail.com <zeus.k99@gmail.com>
2021-11-16 10:04:24 -0400

    bug tests category and products fixed

--- tests/products.test.js
+++ tests/products.test.js
@@ -2,42 +2,25 @@ import supertest from 'supertest';
 import '../src/setup/setup.js';
 import app from '../src/app.js';
 import connection from '../src/database/database.js';
-import { insertAllProducts} from '../src/factories/productsFactory.js';
+import { insertAllProducts } from '../src/factories/productsFactory.js';
+import { insertAllCategories } from '../src/factories/categoriesFactory.js';
 
 const agent = supertest(app);
 
 afterAll(async () => {
-  await connection.query('DELETE FROM products');
+  await connection.query('DELETE FROM products CASCADE;');
+  await connection.query('DELETE FROM categories CASCADE;');
+  await connection.query('ALTER SEQUENCE products_id_seq RESTART WITH 1;');
+  await connection.query('ALTER SEQUENCE categories_id_seq RESTART WITH 1;');
   connection.end();
 });
 
 beforeAll(async () => {
-  await connection.query(
-    `
-    drop table categories cascade;
-    CREATE TABLE "categories" (
-        "id" serial NOT NULL,
-        "name" varchar(255) NOT NULL UNIQUE,
-        "url_image" text NOT NULL,
-        CONSTRAINT "categories_pk" PRIMARY KEY ("id")
-    ) WITH (
-    OIDS=FALSE);
-    
-    ALTER TABLE "products" ADD CONSTRAINT "products_fk1" FOREIGN KEY ("category_id") REFERENCES "categories"("id");
-    
-    INSERT INTO categories (name, url_image) VALUES ('Watch', 'https://s1.1zoom.me/big0/648/Apple_Clock_Watch_Apple_502952.jpg');
-
-    INSERT INTO categories (name, url_image) VALUES ('AirPods', 'https://s2.glbimg.com/5KBEl01R5_6occgjE0vTUTTwyRU=/0x0:1306x1306/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2021/K/k/qVcT6nTRKDuibbd5wsng/apple-airpods-3rd-gen-hero-10182021.jpg');
-
-    INSERT INTO categories (name, url_image) VALUES ('MacBook', 'https://www.apple.com/v/mac/home/bj/images/meta/mac__bfa414svyuc2_og.png?202111031714');
-
-    INSERT INTO categories (name, url_image) VALUES ('Ipad', 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/ipad-air-select-wifi-blue-202009_FMT_WHH?wid=1000&hei=1000&fmt=jpeg&qlt=95&.v=1599672435000');
-
-    INSERT INTO categories (name, url_image) VALUES ('Iphone', 'https://i.zst.com.br/thumbs/12/38/36/1268435165.jpg');
-
-    INSERT INTO categories (name, url_image) VALUES ('Acessories', 'https://photos5.appleinsider.com/archive/appleaccessories.jpg');`
-  );
-
+  await connection.query('DELETE FROM products CASCADE;');
+  await connection.query('DELETE FROM categories CASCADE;');
+  await connection.query('ALTER SEQUENCE products_id_seq RESTART WITH 1;');
+  await connection.query('ALTER SEQUENCE categories_id_seq RESTART WITH 1;');
+  insertAllCategories();
   insertAllProducts();
 });
 
